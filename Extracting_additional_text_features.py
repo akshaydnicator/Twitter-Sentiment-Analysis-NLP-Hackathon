77094e9eab6a62d9fc943c12f34c646ed35a132f
akdnic <akshaykaushal111@gmail.com>
2020-04-21 22:32:57 +0530

    Updated

---
+++ Extracting_additional_text_features.py
@@ -0,0 +1,75 @@
+# Import required libraries
+import numpy as np
+import pandas as pd
+import re
+
+# Load training dataset
+df = pd.read_csv('train.csv')
+print(df.head())
+
+# Function to calculate length of characters in a tweet
+def tweet_length(x):
+    length = len(x)
+    return length
+
+# Calculate and append tweet length to dataframe
+df['tweet_len'] = df['tweet'].apply(lambda x: tweet_length(x))
+
+
+# Create a list of punctuations
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£', 
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', 
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√']
+
+# Function to calculate the total length of punctuation marks in a tweet
+def puncts_len(x):
+    punct_list = []
+    x = str(x)
+    for punct in puncts:
+        for char in x:
+            if punct==char:
+                punct_list.append(punct)
+    return len(punct_list)
+
+# Calculate length of punctuation marks
+df['punct_len'] = df['tweet'].apply(lambda x: puncts_len(x))
+print(df.head())
+
+# Describing the basic statistics including mean, count, std etc for extracted tweet lengths and punctuation marks length
+print(df['tweet_len'].describe())
+print(df['punct_len'].describe())
+
+# Using matlotlib to visualize the distribution of length of tweets
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+bins = 1.15**(np.arange(0,50))
+plt.hist(df['tweet_len'],bins=bins,alpha=0.8)
+plt.hist(df[df['label']==1]['tweet_len'],bins=bins,alpha=0.8)
+plt.legend(('pos','neg'))
+plt.show()
+
+# Visualizing the distribution of length of punctuation marks
+bins = 1.15**(np.arange(0,30))
+plt.hist(df['punct_len'],bins=bins,alpha=0.8)
+plt.hist(df[df['label']==1]['punct_len'],bins=bins,alpha=0.8)
+plt.legend(('pos','neg'))
+plt.show()
+
+# Saving additional features with training dataset for further use
+df.to_csv('train_more_features.csv')
+
+
+# Similarly for test dataset loading new dataframe
+df1 = pd.read_csv('test.csv')
+print(df1.head())
+
+# Calculating tweet length and punctuation marks length in test dataset and adding to dataframe
+df1['tweet_len'] = df1['tweet'].apply(lambda x: tweet_length(x))
+df1['punct_len'] = df1['tweet'].apply(lambda x: puncts_len(x))
+print(df1.head())
+
+# Saving additional features with test dataset for further use
+df1.to_csv('test_more_features.csv')
---
+++ Tweets_to_ELMo_embeddings.py
@@ -0,0 +1,117 @@
+
+#Import required libraries
+import pandas as pd
+import numpy as np
+import spacy
+from tqdm import tqdm
+import re
+import time
+import pickle
+pd.set_option('display.max_colwidth', 100)
+
+# read train and test dataset
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+
+print(train.shape, test.shape)
+
+# Check distribution of positive and negative tweets
+print(train['label'].value_counts(normalize = True))
+
+print(train.head())
+
+# Create a list of punctuation marks
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£', 
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', 
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√']
+
+# Function to replace punctuation marks with whitespaces
+def clean_text(x):
+    x = str(x)
+    for punct in puncts:
+        if punct in x:
+            x = x.replace(punct, ' ')
+    return x
+
+# Standard cleaning process and text normalization
+# Remove URL's from train and test
+train['clean_tweet'] = train['tweet'].apply(lambda x: re.sub(r'http\S+', '', x))
+test['clean_tweet'] = test['tweet'].apply(lambda x: re.sub(r'http\S+', '', x))
+
+# Remove user handles @
+train['clean_tweet'] = train['clean_tweet'].apply(lambda x: re.sub("@[\w]*", '', x))
+test['clean_tweet'] = test['clean_tweet'].apply(lambda x: re.sub("@[\w]*", '', x))
+
+# Remove punctuation marks
+train['clean_tweet'] = train['clean_tweet'].apply(lambda x: clean_text(x))
+test['clean_tweet'] = test['clean_tweet'].apply(lambda x: clean_text(x))
+
+# Convert text to lowercase
+train['clean_tweet'] = train['clean_tweet'].str.lower()
+test['clean_tweet'] = test['clean_tweet'].str.lower()
+
+# Remove numbers
+train['clean_tweet'] = train['clean_tweet'].str.replace("[0-9]", " ")
+test['clean_tweet'] = test['clean_tweet'].str.replace("[0-9]", " ")
+
+# Remove whitespaces
+train['clean_tweet'] = train['clean_tweet'].apply(lambda x:' '.join(x.split()))
+test['clean_tweet'] = test['clean_tweet'].apply(lambda x: ' '.join(x.split()))
+
+# Additional 'Optional' step for text normalization
+# Import spaCy's language model
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+# Function to lemmatize text
+def lemmatization(texts):
+    output = []
+    for i in texts:
+        s = [token.lemma_ for token in nlp(i)]
+        output.append(' '.join(s))
+    return output
+
+# lemmatizing tweets to their basic form for text normalization
+train['clean_tweet'] = lemmatization(train['clean_tweet'])
+test['clean_tweet'] = lemmatization(test['clean_tweet'])
+
+print(train.head())
+
+# Importing tensorflow hub and loading locally saved ELMo embeddings model
+import tensorflow_hub as hub
+import tensorflow as tf
+
+elmo = hub.Module('C:\\Users\\Akshay Kaushal\\Desktop\\Desktop\\Akki\\NLP\\Data Processing\\Hackathon\\ELMo', trainable=True)
+
+# Function to convert tweets to ELMo embeddings
+def elmo_vectors(x):
+  embeddings = elmo(x.tolist(), signature="default", as_dict=True)["elmo"]
+
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.tables_initializer())
+    # return average of ELMo features
+    return sess.run(tf.reduce_mean(embeddings,1))
+
+# Creating batches of 100 tweets to feed into elmo model at a time as it consumes high computation power and memory 
+list_train = [train[i:i+100] for i in range(0,train.shape[0],100)]
+list_test = [test[i:i+100] for i in range(0,test.shape[0],100)]
+
+# Extract ELMo embeddings
+elmo_train = [elmo_vectors(x['clean_tweet']) for x in list_train]
+elmo_test = [elmo_vectors(x['clean_tweet']) for x in list_test]
+
+# Concatenating converted batches into single array of train and test dataset embeddings
+elmo_train_new = np.concatenate(elmo_train, axis = 0)
+elmo_test_new = np.concatenate(elmo_test, axis = 0)
+
+# save elmo_train
+pickle_out = open("elmo_train.pickle","wb")
+pickle.dump(elmo_train_new, pickle_out)
+pickle_out.close()
+
+# save elmo_test
+pickle_out = open("elmo_test.pickle","wb")
+pickle.dump(elmo_test_new, pickle_out)
+pickle_out.close()
